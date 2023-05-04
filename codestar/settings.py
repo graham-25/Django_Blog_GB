cd7d9717269275559c0cc1e4895034450ad8f9d6
graham-25 <grbills29@googlemail.com>
2023-05-04 17:00:53 +0000

    deployment commit

--- codestar/settings.py
+++ codestar/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', 'graham-25-codestar2023.heroku.com']
+ALLOWED_HOSTS = ['localhost', 'graham-25-codestar2023.herokuapp.com']
 
 
 # Application definition
