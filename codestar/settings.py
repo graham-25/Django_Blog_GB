f1cbf4a32b50f5ac4e7ab6ec65962b633752c7bf
graham-25 <grbills29@googlemail.com>
2023-05-22 19:58:44 +0000

    lost files

--- codestar/settings.py
+++ codestar/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', 'graham-25-codestar2023.herokuapp.com']
+ALLOWED_HOSTS = ['graham-25-codestar2023.herokuapp.com', 'localhost']
 
 
 # Application definition
