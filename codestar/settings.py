df6fe30ef9e6be67fbac3a9e900934e2ea0c671a
graham-25 <grbills29@googlemail.com>
2023-05-31 17:17:25 +0000

    on-page messages and js to dismiss after 3 sec

--- codestar/settings.py
+++ codestar/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 import os
 import dj_database_url
+from django.contrib.messages import constants as messages
 if os.path.isfile('env.py'):
     import env
 
@@ -30,7 +31,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['graham-25-codestar2023.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['graham-25-codestar2023.herokuapp.com', 'localhost', '8000-graham25-djangobloggb-x5ls8r7jt5d.ws-eu98.gitpod.io']
 
 
 # Application definition
@@ -57,6 +58,14 @@ SITE_ID = 1
 
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
+
+MESSAGE_TAGS = {
+    messages.DEBUG: 'alert-info',
+    messages.INFO: 'alert-info',
+    messages.SUCCESS: 'alert-success',
+    messages.WARNING: 'alert-warning',
+    messages.ERROR: 'alert-danger',
+}
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
